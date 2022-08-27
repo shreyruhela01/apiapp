a260bd3a25dbe0407eaf3b7c392b285d449bf1e2
shreyruhela01 <shreyruhela1311@gmail.com>
2022-08-27 15:02:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import MultiSelectComponent from './MultiSelectComponent'
+function App() {  
+    
+  return (  
+    <div className="App">  
+      <MultiSelectComponent />  
+    </div>  
+  );  
+}  
+export default App;
